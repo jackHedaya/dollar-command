646b121fe52d701623869df65229766bb4090887
Jack Hedaya <jackehedaya@gmail.com>
2019-09-01 13:05:05 -0400

    added logic script

--- lib/dollar.rb
+++ lib/dollar.rb
@@ -1,6 +1,20 @@
 require "dollar/version"
 
 module Dollar
-  class Error < StandardError; end
-  # Your code goes here...
+  class Dollar
+    def start
+      arguments = ARGV
+
+      if arguments[0] == "$" 
+        arguments.shift
+      elsif arguments.length < 1 || arguments[0] == "--help" 
+        puts '$: Runs any commands that follow it to help with copy and paste errors'
+        exit
+      end
+
+      exec arguments.join(' ')
+    end
+  end
 end
+
+puts Dollar::Dollar.new.start
